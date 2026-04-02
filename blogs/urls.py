790bbbce282eec1181d70123a33677d285cea5d8
Joevan <joevan@qq.com>
2026-04-03 02:36:09 +0800

    修复index

--- blogs/urls.py
+++ blogs/urls.py
@@ -19,7 +19,7 @@ def main_site_only(view_func):
     return _wrapped_view
 
 urlpatterns = [
-    path('', main_site_only(blog.index), name='index'),
+    path('', main_site_only(discover.discover), name='index'),
     # path('', blog.home, name='home'),
     path('home/', blog.home, name='home'),
     path('logger-test/', logger.logger_test),
